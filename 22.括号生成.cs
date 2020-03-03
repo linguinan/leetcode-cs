995b4b296695846ee4c9392a632209f2facd37e0
Guinan Lin <linguinan@126.com>
2020-03-03 22:48:49 +0800

    第二种解法

--- 22.括号生成.cs
+++ 22.括号生成.cs
@@ -7,15 +7,15 @@ using System.Collections.Generic;
 
 // @lc code=start
 public class Solution {
-    public IList<string> GenerateParenthesis(int n) {
+    public IList<string> GenerateParenthesis2(int n) {
         IList<string> list = new List<string>();
         if (n <= 0)
             return list;
-        gen(list, 0, 0, n, "");
+        gen2(list, 0, 0, n, "");
         return list;
     }
 
-    void gen(IList<string> list, int left, int right, int n, string res) {
+    void gen2(IList<string> list, int left, int right, int n, string res) {
         if (left == n && right == n)
         {
             list.Add(res);
@@ -25,11 +25,36 @@ public class Solution {
         if (left < n)
         {
             // 注意：不能用++left 这样会修改left值影响后续的递归
-            gen(list, left + 1, right, n, res + "(");
+            gen2(list, left + 1, right, n, res + "(");
         }
         if (left > right && right < n)
         {
-            gen(list, left, right + 1, n, res + ")");
+            gen2(list, left, right + 1, n, res + ")");
+        }
+    }
+
+    public IList<string> GenerateParenthesis(int n) {
+        IList<string> list = new List<string>();
+        if (n <= 0)
+            return list;
+        gen(list, n, n, "");
+        return list;
+    }
+
+    void gen(IList<string> list, int left, int right, string res) {
+        if (left == 0 && right == 0)
+        {
+            list.Add(res);
+            return;
+        }
+
+        if (left > 0)
+        {
+            gen(list, left - 1, right, res + "(");
+        }
+        if (right > left)
+        {
+            gen(list, left, right - 1, res + ")");
         }
     }
 }
