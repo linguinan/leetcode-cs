5f9ba766662b4340e1c86ad936a334031113ec3e
Guinan Lin <linguinan@126.com>
2020-03-03 22:41:37 +0800

    done

--- 22.括号生成.cs
+++ 22.括号生成.cs
@@ -11,12 +11,26 @@ public class Solution {
         IList<string> list = new List<string>();
         if (n <= 0)
             return list;
-        gen(list, n, n);
+        gen(list, 0, 0, n, "");
         return list;
     }
 
-    void gen(IList<string> list, int left, int right) {
-        
+    void gen(IList<string> list, int left, int right, int n, string res) {
+        if (left == n && right == n)
+        {
+            list.Add(res);
+            return;
+        }
+
+        if (left < n)
+        {
+            // 注意：不能用++left 这样会修改left值影响后续的递归
+            gen(list, left + 1, right, n, res + "(");
+        }
+        if (left > right && right < n)
+        {
+            gen(list, left, right + 1, n, res + ")");
+        }
     }
 }
 // @lc code=end
