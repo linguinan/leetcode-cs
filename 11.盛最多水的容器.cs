828f5ed50e9b1cc840987cfc07531de7cc25ea9f
Guinan Lin <linguinan@126.com>
2020-03-11 00:28:09 +0800

    优化

--- 11.盛最多水的容器.cs
+++ 11.盛最多水的容器.cs
@@ -8,15 +8,27 @@
 using System;
 
 public class Solution {
+    // public int MaxArea(int[] height) {
+    //     int max = 0;
+    //     for (int i = 0; i < height.Length; i++)
+    //     {
+    //         for (int j = i + 1; j < height.Length; j++)
+    //         {
+    //             int area = (j - i) * Math.Min(height[i], height[j]);
+    //             max = Math.Max(max, area);
+    //         }
+    //     }
+    //     return max;
+    // }
+
     public int MaxArea(int[] height) {
         int max = 0;
-        for (int i = 0; i < height.Length; i++)
+        for (int i = 0, j = height.Length - 1; i < j;)
         {
-            for (int j = i + 1; j < height.Length; j++)
-            {
-                int area = (j - i) * Math.Min(height[i], height[j]);
-                max = Math.Max(max, area);
-            }
+            // 只移动小的下标，所以下面计算只需要+1
+            int h = height[i] < height[j] ? height[i++] : height[j--];
+            int area = (j - i + 1) * h;
+            max = Math.Max(max, area);
         }
         return max;
     }
