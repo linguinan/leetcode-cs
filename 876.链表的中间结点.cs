6928599adbdc56516905cd400d234a03fb4edbbe
Guinan Lin <linguinan@126.com>
2020-03-23 18:02:32 +0800

    add

---
+++ 876.链表的中间结点.cs
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=876 lang=csharp
+ *
+ * [876] 链表的中间结点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode MiddleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
+// @lc code=end
+
