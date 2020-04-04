e68503b7af4f36045007a9091ac0bfd3331d420a
Guinan Lin <linguinan@126.com>
2020-04-04 22:33:31 +0800

    打卡每日一题

--- 42.接雨水.cs
+++ 42.接雨水.cs
@@ -14,24 +14,22 @@ public class Solution {
     /// 用栈循环遍历右边界
     /// 容量就是左右边界之宽*最矮的边，如果没有左或右边界则跳过 
     /// </code>
-    public int Trap(int[] height) {
-        Stack<int> stack = new Stack<int>();
+    public int Trap (int[] height) {
+        Stack<int> stack = new Stack<int> ();
         int ans = 0;
-        for (int i = 0; i < height.Length; i++)
-        {
-            while (stack.Count > 0 && height[i] > height[stack.Peek()])
-            {
-                int top = stack.Pop();
-                if (stack.Count <= 0)
-                    break;
-                int dis = i - stack.Peek() - 1;
-                int hig = Math.Min(height[i], height[stack.Peek()]) - height[top];
-                ans += hig * dis;
+        for (int i = 0; i < height.Length; i++) {
+            while (stack.Count > 0 && height[i] >= height[stack.Peek ()]) {
+                int top = stack.Pop ();
+                if (stack.Count > 0) {
+                    // i与前一个柱子构成凹槽，减去凹柱高度
+                    int w = i - stack.Peek () - 1;
+                    int h = Math.Min (height[stack.Peek ()], height[i]) - height[top];
+                    ans += w * h;
+                }
             }
-            stack.Push(i);
+            stack.Push (i);
         }
         return ans;
     }
 }
 // @lc code=end
-
