a2c41d304e253e830b1612c56fceda89c2586b21
Guinan Lin <linguinan@126.com>
2020-04-15 23:07:25 +0800

    优化耗时

--- 542.01-矩阵.cs
+++ 542.01-矩阵.cs
@@ -10,21 +10,24 @@ using System.Collections.Generic;
 public class Solution {
     /// <summary>
     /// BFS 从所有0元素出发遍历累加
+    /// 把visited改为用值判断，耗时由560->436ms，beats 88%
     /// </summary>
     /// <param name="matrix"></param>
     /// <returns></returns>
     public int[][] UpdateMatrix (int[][] matrix) {
         int m = matrix.Length, n = matrix[0].Length;
         int[][] arr = new int[m][];
-        bool[][] visited = new bool[m][];
+        // bool[][] visited = new bool[m][];
         Queue<KeyValuePair<int, int>> queue = new Queue<KeyValuePair<int, int>> ();
         for (int i = 0; i < m; i++) {
             arr[i] = new int[n];
-            visited[i] = new bool[n];
+            // visited[i] = new bool[n];
             for (int j = 0; j < n; j++) {
                 if (matrix[i][j] == 0) {
                     queue.Enqueue (new KeyValuePair<int, int> (i, j));
-                    visited[i][j] = true;
+                    // visited[i][j] = true;
+                } else {
+                    arr[i][j] = int.MaxValue;
                 }
             }
         }
@@ -36,10 +39,12 @@ public class Solution {
                 di = item.Key + dirs[i][0];
                 dj = item.Value + dirs[i][1];
                 if (di < 0 || di >= m || dj < 0 || dj >= n) continue;
-                if (visited[di][dj]) continue;
-                arr[di][dj] = 1 + arr[item.Key][item.Value];
-                visited[di][dj] = true;
-                queue.Enqueue (new KeyValuePair<int, int> (di, dj));
+                // if (visited[di][dj]) continue;
+                if (arr[di][dj] > 1 + arr[item.Key][item.Value]) {
+                    arr[di][dj] = 1 + arr[item.Key][item.Value];
+                    // visited[di][dj] = true;
+                    queue.Enqueue (new KeyValuePair<int, int> (di, dj));
+                }
             }
         }
         return arr;
