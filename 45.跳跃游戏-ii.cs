667af9e0b2665d834de1cb2879d1ea674bbe5086
Guinan Lin <linguinan@126.com>
2020-05-04 23:21:31 +0800

    打卡每日一题

--- 45.跳跃游戏-ii.cs
+++ 45.跳跃游戏-ii.cs
@@ -8,6 +8,12 @@
 using System;
 
 public class Solution {
+    /// <summary>
+    /// 在当前位置可到达的最远范围内，找出可跳跃最远的距离
+    /// (i == end) 已达最远，继续下个区间
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
     public int Jump (int[] nums) {
         int ans = 0, end = 0, max = 0;
         for (int i = 0; i < nums.Length - 1; i++) {
