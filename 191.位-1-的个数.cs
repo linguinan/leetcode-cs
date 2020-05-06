16e5622d0a9e8befbfef27b0dfed4e09076967dd
Guinan Lin <linguinan@126.com>
2020-05-07 00:26:44 +0800

    与运算清掉最低位

--- 191.位-1-的个数.cs
+++ 191.位-1-的个数.cs
@@ -6,11 +6,20 @@
 
 // @lc code=start
 public class Solution {
-    public int HammingWeight (uint n) {
+    public int HammingWeight2 (uint n) {
         int cnt = 0;
         while (n > 0) {
             if ((n & 1) == 1) cnt++;
             n = n >> 1;
+        }
+        return cnt;
+    }
+
+    public int HammingWeight (uint n) {
+        int cnt = 0;
+        while (n > 0) {
+            cnt++;
+            n &= (n - 1);//清掉最低位的1
         }
         return cnt;
     }
