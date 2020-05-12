3feb7c328330c09ae8b10c0320136cd440d60d33
Guinan Lin <linguinan@126.com>
2020-05-12 23:26:13 +0800

    打卡

--- 155.最小栈.cs
+++ 155.最小栈.cs
@@ -13,32 +13,35 @@ public class MinStack {
     private Stack<int> helperStack;
 
     /** initialize your data structure here. */
-    public MinStack() {
-        dataStack = new Stack<int>();
-        helperStack = new Stack<int>();
+    public MinStack () {
+        dataStack = new Stack<int> ();
+        helperStack = new Stack<int> ();
     }
-    
-    public void Push(int x) {
-        dataStack.Push(x);
-        if (helperStack.Count == 0 || helperStack.Peek() > x)
-        {
-            helperStack.Push(x);
+
+    /// <summary>
+    /// 关键点，理解辅助栈，在入栈元素大于栈顶元素时，栈顶元素再次入栈
+    /// </summary>
+    /// <param name="x"></param>
+    public void Push (int x) {
+        dataStack.Push (x);
+        if (helperStack.Count == 0 || helperStack.Peek () > x) {
+            helperStack.Push (x);
         } else {
-            helperStack.Push(helperStack.Peek());
+            helperStack.Push (helperStack.Peek ());
         }
     }
-    
-    public void Pop() {
-        dataStack.Pop();
-        helperStack.Pop();
+
+    public void Pop () {
+        dataStack.Pop ();
+        helperStack.Pop ();
     }
-    
-    public int Top() {
-        return dataStack.Peek();
+
+    public int Top () {
+        return dataStack.Peek ();
     }
-    
-    public int GetMin() {
-        return helperStack.Peek();
+
+    public int GetMin () {
+        return helperStack.Peek ();
     }
 }
 
@@ -52,3 +55,16 @@ public class MinStack {
  */
 // @lc code=end
 
+static int Main(string[] args)
+{
+    MinStack stack = new MinStack();
+    stack.Push(-2);
+    stack.Push(0);
+    stack.Push(-3);
+    System.Console.WriteLine(stack.GetMin());//-3
+    stack.Pop();
+    System.Console.WriteLine(stack.Top());//0
+    System.Console.WriteLine(stack.GetMin());//-2
+    return 0;
+}
+Main (null);
