a54173b592a55019aa36c2428049a83def4dc432
Guinan Lin <linguinan@126.com>
2020-06-14 23:28:20 +0800

    test

--- Test.cs
+++ Test.cs
@@ -11,10 +11,12 @@ public class Test {
 static int Main(string[] args)
 {
     // System.Console.WriteLine();
-    new Test().Print("123456");
+    // new Test().Print("123456");
+    int a = 7, b = 6;
+    System.Console.WriteLine((a & b) == 0);
     return 0;
 }
 Main(null);
 
 
-new Test().Print("zzzzzz");
+// new Test().Print("zzzzzz");
