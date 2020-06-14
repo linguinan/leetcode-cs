57db64bd0e842ab028d50cd4aa7ce1e5ad19b5dc
Guinan Lin <linguinan@126.com>
2020-06-14 22:38:57 +0800

    递推的实现

--- 面试题13.机器人的运动范围.cs
+++ 面试题13.机器人的运动范围.cs
@@ -5,7 +5,10 @@ public class Solution {
     private int m, n, k;
     private bool[][] visited;
     /// <summary>
-    /// 深度优先搜索
+    /// 深度优先搜索 DFS    O(MN)
+    /// 数位和增量公式:
+    /// 19，20 = 10 - 8 则为 x - 8
+    /// 1，2 = 1 + 1 则为 x + 1
     /// </summary>
     /// <param name="m"></param>
     /// <param name="n"></param>
@@ -22,6 +25,9 @@ public class Solution {
         return dfs (0, 0, 0, 0);
     }
 
+    /// <param name="si">i 的数位和</param>
+    /// <param name="sj">j 的数位和</param>
+    /// <returns></returns>
     public int dfs (int i, int j, int si, int sj) {
         if (i >= m || j >= n || k < (si + sj) || visited[i][j]) return 0;
         visited[i][j] = true;
@@ -29,13 +35,13 @@ public class Solution {
     }
 
     /// <summary>
-    /// 广度优先搜索
+    /// 广度优先搜索 BFS    O(MN)
     /// </summary>
     /// <param name="m"></param>
     /// <param name="n"></param>
     /// <param name="k"></param>
     /// <returns></returns>
-    public int MovingCount (int m, int n, int k) {
+    public int MovingCount3 (int m, int n, int k) {
         bool[][] visited = new bool[m][];
         for (int i = 0; i < m; i++) {
             visited[i] = new bool[n];
@@ -45,7 +51,7 @@ public class Solution {
         queue.Enqueue (new KeyValuePair<int, int> (0, 0));
         int ans = 1;
         while (queue.Count > 0) {
-            int size = queue.Count;//每次都把queue清空会计算更快
+            int size = queue.Count; //每次都把queue清空会计算更快
             for (int i = 0; i < size; i++) {
                 var node = queue.Dequeue ();
                 for (int j = 0; j < 2; j++) {
@@ -56,6 +62,34 @@ public class Solution {
                     visited[dx][dy] = true;
                     ans++;
                 }
+            }
+        }
+        return ans;
+    }
+
+    /// <summary>
+    /// 递推    O(MN)
+    /// 在i，j的数位之和不大于K的情况下
+    /// 【i，j】是否能到达取决于其前一个位置是否能到达
+    /// </summary>
+    /// <param name="m"></param>
+    /// <param name="n"></param>
+    /// <param name="k"></param>
+    /// <returns></returns>
+    public int MovingCount (int m, int n, int k) {
+        int[][] visited = new int[m][];
+        for (int i = 0; i < m; i++) {
+            visited[i] = new int[n];
+        }
+        int ans = 1;
+        visited[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if ((i == 0 && j == 0) || sum(i) + sum(j) > k) continue;
+                // | 或运算，只要有一个为1则结果为1
+                if (i - 1 >= 0) visited[i][j] |= visited[i - 1][j];
+                if (j - 1 >= 0) visited[i][j] |= visited[i][j - 1];
+                ans += visited[i][j];
             }
         }
         return ans;
@@ -79,7 +113,7 @@ public class Solution {
 static int Main (string[] args) {
     Solution solution = new Solution ();
     // int res = solution.MovingCount (7, 2, 3);//7
-    int res = solution.MovingCount (38, 15, 9);//135
+    int res = solution.MovingCount (38, 15, 9); //135
     System.Console.WriteLine (res);
     return 0;
 }
